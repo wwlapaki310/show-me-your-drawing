6ecb6cba1d79edecb7574bfd5da5b37f40474bcd
aki310 <wwlap24@gmail.com>
2019-09-01 18:18:40 +0900

    helloworld

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import request
 from PIL import Image
 from PIL import ImageDraw, ImageFont
 from collections import namedtuple
-import cv2
 import math
 from requests_oauthlib import OAuth1Session
 import json
